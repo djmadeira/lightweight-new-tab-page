04ebc81a11f9e936e453a4cce0aa8db36e4c13c0
DJ Madeira <dj@livemercury.com>
2014-10-18 22:15:10 -0400

    Added custom links, CSS customization.

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,20 @@
 window.addEventListener('DOMContentLoaded', newTabPageMain);
 
 function newTabPageMain () {
-
   var internalLink = /^chrome:/;
+  var customLinks = document.getElementById('custom-links');
+  var links = document.querySelectorAll('a');
+  var overlayToggles = document.querySelectorAll('.overlay-toggle');
+  var customCSS = document.getElementById('custom-css');
+  var customCSSField = document.getElementById('custom-css-field')
+  var editLinksButton = document.getElementById('edit-links-toggle');
+  var customLinks = JSON.parse(localStorage.getItem('newtab-custom-links')) ||
+      [ // Default
+        ['Twitter', 'https://twitter.com']
+      ];
+  var customLinksUl = document.getElementById('custom-links');
+  var linkTemplate = document.getElementById('custom-link-template');
+
   function handleLinkEvent (e) {
     e.preventDefault();
     chrome.tabs.getCurrent(function (tab) {
@@ -12,11 +24,99 @@ function newTabPageMain () {
     });
   }
 
-  var links = document.querySelectorAll('a');
+  function replaceTemplateTags(templateEl, tagsObject) {
+    var src = templateEl.innerHTML,
+        dest = document.createElement('template');
+
+    for (var tag in tagsObject) {
+      src = src.replace('{{'+tag+'}}', tagsObject[tag]);
+    }
+
+    dest.innerHTML = src;
+    return dest.content.cloneNode(true);
+  }
+
+  function enableLinkEditing () {
+    customLinks = document.getElementById('custom-links')
+    customLinks.classList.add('is-editing');
+    appendNewLinkField();
+  }
+
+  function disableLinkEditing () {
+    customLinks = document.getElementById('custom-links')
+
+    customLinks.classList.remove('is-editing');
+    customLinks.removeChild(document.getElementById('new-link'));
+  }
+
+  function appendNewLinkField () {
+    customLinks = document.getElementById('custom-links');
+    customLinks.appendChild(document.getElementById('new-link-template').content.cloneNode(true));
+    // When link is submitted
+    document.getElementById('new-link-confirm').addEventListener('click', function (e) {
+      var title = document.getElementById('new-link-title').value,
+          href = document.getElementById('new-link-href').value;
+     
+      addNewLink([title, href]);
+
+      document.getElementById('new-link-title').value = '';
+      document.getElementById('new-link-href').value = '';
+    });
+  }
+
+  function addNewLink(link) {
+    var currentLinks = JSON.parse(localStorage.getItem('newtab-custom-links'));
+
+    currentLinks.push(link);
+    localStorage.setItem('newtab-custom-links', JSON.stringify(currentLinks));
+
+    var newItem = replaceTemplateTags(linkTemplate, {
+      'title': link[0],
+      'href': link[1]
+    });
+
+    bindLinkRemoveHandler(newItem.querySelector('.edit-button.remove'));
+
+    customLinks.insertBefore(newItem, document.getElementById('new-link'));
+  }
+
+  function bindLinkRemoveHandler (el) {
+    var eventListener = function (e) {
+      customLinks = JSON.parse(localStorage.getItem('newtab-custom-links'));
+      var title = this.parentElement.querySelector('.custom-link').innerHTML;
+      for (var i=0; i < customLinks.length; i++) {
+        if (customLinks[i][0] == title) {
+          console.log(i);
+          customLinks.splice(i, 1);
+          break;
+        }
+      }
+
+      localStorage.setItem('newtab-custom-links', JSON.stringify(customLinks));
+
+      this.parentElement.parentElement.removeChild(this.parentElement);
+    }
+
+    if (el) {
+      el.addEventListener('click', eventListener);
+    } else {
+      var els = document.querySelectorAll('#custom-links .edit-button.remove');
+      for (var i=0; i < els.length; i++) {
+        els[i].addEventListener('click', eventListener);
+      }
+    }
+  }
+
   for (var i=0, j=links.length; i<j; i++) {
     if (links[i].href.match(internalLink)) {
       links[i].addEventListener('click', handleLinkEvent);
     }
+  }
+
+  for (var i=0, j=overlayToggles.length; i<j; i++) {
+    overlayToggles[i].addEventListener('click', function (e) {
+      this.parentElement.classList.toggle('is-active');
+    })
   }
 
   document.getElementById('clear-cache').addEventListener('click', function () {
@@ -68,4 +168,37 @@ function newTabPageMain () {
     });
   });
 
+  // CSS customizer setup
+  customCSS.innerHTML = localStorage.getItem('newtab-custom-css');
+  customCSSField.value = customCSS.innerHTML;
+
+  customCSSField.addEventListener('keyup', function (e) {
+    document.getElementById('custom-css').innerHTML = this.value;
+    localStorage.setItem('newtab-custom-css', this.value);
+  });
+
+  // Custom links setup
+  for (var i=0, j=customLinks.length; i<j; i++) {
+    var link = replaceTemplateTags(linkTemplate, {
+      'title': customLinks[i][0],
+      'href': customLinks[i][1]
+    });
+
+    customLinksUl.appendChild(link);
+  }
+
+  bindLinkRemoveHandler();
+
+  // Custom links UI
+  editLinksButton.addEventListener('click', function (e) {
+    if (this.classList.contains('is-active')) {
+      disableLinkEditing();
+      this.classList.remove('is-active');
+      this.innerHTML = 'Edit links';
+    } else {
+      enableLinkEditing();
+      this.classList.add('is-active');
+      this.innerHTML = 'Done editing'
+    }
+  })
 }
--- readme.md
+++ readme.md
@@ -2,8 +2,21 @@
 
 I don't like the default new tab, and I haven't found a different one that added any more value for me than the default. So I made a ridiculously simple page of my own. 
 
-<img src="screenshot.jpg">
+<img src="screenshot.png">
+
+# Installation
+
+To install, download `chrome-new-tab-extension.crx`. Go to chrome://extensions (just paste that in your address bar.) Drag the file into that window.
 
 It loads in around 50ms if it hasn't been loaded in a while (the default takes around 500). This may not matter to some people, but to me it's important. Plus, I don't need it phoning home every time I open a new browser tab.
 
 I plan to add some lightweight customization options in the future, so you can add your own quick links, and a nice recently closed tab recovery section. But it's never going to get a big feature list with a weather widget and flickr photos and that nonsense. It's just going to be a straightforward new tab page for people who like that sort of thing.
+
+# Changelog
+
+## 1.1
+* Added customizable links
+* Added custom CSS
+
+## 1.0
+* Initial release
